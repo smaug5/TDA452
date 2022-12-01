b19487a095a1c6e2286ea7ee3025f928fe9dfea5
Wanda Wannelöf - Smaug <wannelof@student.chalmers.se>
2022-12-01 14:46:09 +0100

    Added sudoku-files
    Added files with example sudokus

--- Lab3/Sudoku.hs
+++ Lab3/Sudoku.hs
@@ -4,6 +4,7 @@ import Test.QuickCheck
 import Data.Maybe
 import Data.List
 import Data.Char
+import GHC.Plugins (subDVarSet, DynFlags (fileSettings), OverlapFlag (isSafeOverlap))
 
 ------------------------------------------------------------------------------
 
@@ -54,7 +55,7 @@ ex2 =
 
 -- * A1
 
--- | allBlankSudoku is a sudoku with just blanks
+-- | allBlankSudoku makes a sudoku with just blanks
 allBlankSudoku :: Sudoku
 allBlankSudoku = Sudoku $ replicate 9 $ replicate 9 n
   where
@@ -66,10 +67,11 @@ allBlankSudoku = Sudoku $ replicate 9 $ replicate 9 n
 -- | isSudoku sud checks if sud is really a valid representation of a sudoku
 -- puzzle
 isSudoku :: Sudoku -> Bool
-isSudoku sud = length(rows sud) == 9 && and [length row == 9 | row <- rows sud] && all isCell cs
+isSudoku sud = length(rows sud) == 9 && and rowLength && all isCell cs
   where 
     rs = rows sud
     cs = concat rs
+    rowLength = [length row == 9 | row <- rows sud]
 
 
 isCell :: Cell -> Bool
@@ -81,7 +83,7 @@ isCell (Just i) | i `elem` [1..9] = True
 -- | isFilled sud checks if sud is completely filled in,
 -- i.e. there are no blanks
 isFilled :: Sudoku -> Bool
-isFilled = and . map isJust . concat . rows
+isFilled = all isJust . concat . rows
 
 ------------------------------------------------------------------------------
 
@@ -94,17 +96,12 @@ printSudoku sud = putStr concatenatedString
   where
     rowsAsStrings = map (concatMap cellToString) (rows sud)
     concatenatedString = concat $ intersperse "\n" rowsAsStrings
-     
+
+-- Represents Cells with strings instead of Cell
 cellToString :: Cell -> String
 cellToString Nothing = "."
 cellToString (Just i) = show i
 
--- rowToString2 :: [Cell] -> String
--- rowToString2 = concatMap cellToString
-
--- rowToString :: Row -> String
--- rowToString [] = ""
--- rowToString (c:cs) = cellToString c ++ rowToString cs
 -- * B2
 
 -- | readSudoku file reads from the file, and either delivers it, or stops
@@ -114,7 +111,11 @@ readSudoku file = do
             content <- readFile file
             let ss = lines content
             let rs = stringToRows ss
-            return $ Sudoku rs
+            let sud = Sudoku rs
+            if (isSudoku sud)
+              then return sud
+              else return $ error "Not a Sudoku!"
+
 
 
 stringToRows :: [String] -> [Row]
@@ -166,7 +167,8 @@ isOkayBlock block = length block == 9 && isNotDuplicate block
 isNotDuplicate :: Block -> Bool
 isNotDuplicate [] = True
 isNotDuplicate (Nothing:smallblock) = isNotDuplicate smallblock
-isNotDuplicate (cell:smallblock) = notElem cell smallblock && isNotDuplicate smallblock
+isNotDuplicate (cell:smallblock) = notElem cell smallblock &&
+                                   isNotDuplicate smallblock
 
 
 -- * D2
@@ -181,7 +183,8 @@ blockBlocks :: Sudoku -> [Block]
 blockBlocks sud = [blockBlock x y sud | x <- [0..2], y <- [0..2]]
 
 blockBlock :: Int -> Int -> Sudoku -> Block
-blockBlock x y sud = concatMap (take 3 . drop (3 * x)) ((take 3 . drop (3 * y)) $ rows sud)
+blockBlock x y sud = concatMap (take 3 . drop (3 * x))
+                               ((take 3 . drop (3 * y)) $ rows sud)
 
 prop_blocks_lengths :: Sudoku -> Bool
 prop_blocks_lengths sudoku = length (blocks sudoku) == 27 && 
@@ -204,40 +207,112 @@ type Pos = (Int,Int)
 
 -- * E1
 
+-- Makes a list with all the positions of blank cells
 blanks :: Sudoku -> [Pos]
-blanks = undefined
+blanks (Sudoku rows) = [(r,c) | r <- [0..8], c <- [0..8],
+                        isNothing (rows !! r !! c)]
 
---prop_blanks_allBlanks :: ...
---prop_blanks_allBlanks =
+
+-- checks so that blanks actually return all blank positions
+prop_blanks_allBlanks :: Bool
+prop_blanks_allBlanks = isSudoku allBlankSudoku &&
+                        (length (blanks allBlankSudoku) == 81)
 
 
 -- * E2
 
+-- Changes a cell into a new value, sends error if index is out of bound
 (!!=) :: [a] -> (Int,a) -> [a]
-xs !!= (i,y) = undefined
+xs !!= (i,y)
+    | null xs = error "List is empty"
+    | length xs < i || i < 0 = error "Index out of bounds"
+    | otherwise = take i xs ++ [y] ++ drop (i+1) xs
 
---prop_bangBangEquals_correct :: ...
---prop_bangBangEquals_correct =
+
+-- checks that the index is valid nad not out of bound
+validIndex :: [a] -> Int -> Bool
+validIndex xs i = length xs <= i || null xs || i <  0
+
+-- checks so that !!= updates a list on the correct position
+-- and nothing else
+prop_bangBangEquals_correct :: Eq a => [a] -> (Int, a) -> Property
+prop_bangBangEquals_correct org (i,y) = not (validIndex org i) ==>
+                             length banged == length org && banged !! i == y
+     where banged = org !!= (i,y)
+     
 
 
 -- * E3
-
+-- updates a sudoku with a new value in a certain position
 update :: Sudoku -> Pos -> Cell -> Sudoku
-update = undefined
+update sud (row, col) newValue = Sudoku $ rows sud !!= 
+                                  (row, rows sud !! row !!= (col, newValue))
 
---prop_update_updated :: ...
---prop_update_updated =
+-- checks so that update updates a sudoku at the correct position
+-- and nothing else
+prop_update_updated :: Sudoku -> Pos -> Cell -> Property
+prop_update_updated sud (row, col) value = 
+  (9 > row && row >= 0) && (9 > col && col >= 0) ==> 
+    Sudoku (rows sud !!= (row, newRow)) == update sud (row, col) value
+  where newRow = (!!=) (rows sud !! row) (col, value)
 
 
 ------------------------------------------------------------------------------
 
 -- * F1
+-- using A2 and D3 (isFilled & isOkay)
+
+-- Solves a sudoku by testing to put in numbers in each blank space
+solve :: Sudoku -> Maybe Sudoku
+solve sud 
+    | null solutions = Nothing
+    | otherwise      = Just $ head solutions
+  where solutions    = solve' (blanks sud) sud
+
+-- helperfunction for solve by also taking in a list of all blank spaces
+solve' :: [Pos] -> Sudoku -> [Sudoku]
+solve' [] sud
+    | isOkay sud = [sud]
+    | otherwise  = [] 
+solve' (pos:positions) sud 
+    | isOkay sud && isFilled sud        = [sud]
+    | isOkay sud && not (isFilled sud)  = concatMap (solve' positions)
+                               [update sud pos val | val <- map Just [1..9]]
+    | otherwise                         = []
 
 
 -- * F2
 
+-- reads a sudoku from a file and prints out the solved sudoku
+readAndSolve :: FilePath -> IO ()
+readAndSolve file = do
+    let iosud = readSudoku file
+    sud <- iosud
+    printSudoku $ fromJust (solve sud)
+
 
 -- * F3
 
+-- checks if a given sudoku is a solution for an unsolved sudoku
+isSolutionOf :: Sudoku -> Sudoku -> Bool
+isSolutionOf sud1 sud2 = isOkay sud1 && isFilled sud1 && isPartOf sud1 sud2
+
+-- checks that the solved sudoku contains same numbers as
+-- an unsolved sudoku
+isPartOf :: Sudoku -> Sudoku -> Bool
+isPartOf sud1 sud2 = isPartOf' sud1 sud2 (blanks sud2)
+
+-- helperfunction for isPartOf which puts in the same empty spaces
+-- as the unsolved sudoku in the solved soduko
+isPartOf' :: Sudoku -> Sudoku -> [Pos] -> Bool
+isPartOf' sud1 sud2 emptySpaces 
+  | null emptySpaces  = sud1 == sud2
+  | otherwise         = isPartOf' (update sud1 (head emptySpaces) Nothing)
+                            sud2 (tail emptySpaces)
 
 -- * F4
+-- checks so that solve acutally makes valid solutions for sudokus
+prop_SolveSound :: Sudoku -> Property
+prop_SolveSound sud = isJust solvedSud ==>
+                      isSolutionOf (fromJust solvedSud) sud
+  where solvedSud = solve sud
